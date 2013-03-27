c176dfddd6794589fd135230fd4b87d5466d7616
GoFrendi Gunawan <gofrendiasgard@gmail.com>
2013-03-27 21:53:20 +0700

    session is absolutely working :)

--- application/index/controllers/index.py
+++ application/index/controllers/index.py
@@ -1,10 +1,15 @@
-from kokoropy.bottle import route
+from kokoropy.bottle import route, request
 
 # modify this file as you want
 
 @route('/')
 def index():
+    if 'counter' in request.session:
+        request.session['counter'] += 1
+    else:
+        request.session['counter'] = 1
     return '''
         <title>Kokoropy</title>
         <h1>It works</h1>
+        <h3>You have visit this page '''+str(request.session['counter'])+''' times</h3>
         <a href="/example/recommended">Check the example</a>'''
