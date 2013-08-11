acdfc72c63f36771ec4e7b0ff68b8313a39cee9a
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-08-11 13:40:43 +0700

    hopefully run

--- heroku_app.py
+++ heroku_app.py
@@ -1,14 +1,19 @@
 import os, sys, tempfile
-os.chdir(os.path.dirname(__file__))
-if os.path.dirname(__file__) not in sys.path:
-    sys.path.append(os.path.dirname(__file__))
+
+PWD = './'
+if os.path.dirname(__file__) == '':
+    PWD = os.getcwd()
+else:
+    PWD = os.path.dirname(__file__)
+os.chdir(PWD)
+if PWD not in sys.path:
+    sys.path.append(PWD)
 
 import kokoropy
 
-PWD = os.path.dirname(os.path.abspath(__file__))
 APP_DIRECTORY = 'applications'
 APPLICATION_PATH = os.path.join(PWD, APP_DIRECTORY)
-application = kokoropy.kokoro_init(server='gevent', port=os.environ.get('PORT', 5000),
+application = kokoropy.kokoro_init(port=os.environ.get('PORT', 5000),
                                    application_path = APPLICATION_PATH, run = True, 
                                    runtime_path = os.path.join(tempfile.gettempdir(), '.heroku_runtime/'), 
                                    base_url = '/')
