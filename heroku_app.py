9c60aefd1ec7f3ff6c55206eb3ba37c45b4fbf81
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-08-11 12:55:44 +0700

    add new line \

--- heroku_app.py
+++ heroku_app.py
@@ -6,7 +6,7 @@ import os, tempfile
 
 import kokoropy
 
-application = kokoropy.kokoro_init(server='gevent', port=os.environ.get('PORT', 5000),
-                                   application_path = './applications', run = True, 
-                                   runtime_path = os.path.join(tempfile.gettempdir(), '.heroku_runtime/'), 
+application = kokoropy.kokoro_init(server='gevent', port=os.environ.get('PORT', 5000), \
+                                   application_path = './applications', run = True, \
+                                   runtime_path = os.path.join(tempfile.gettempdir(), '.heroku_runtime/'), \
                                    base_url = '/')
