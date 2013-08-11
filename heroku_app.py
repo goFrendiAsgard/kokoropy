8d039d553709027e462252b95768143e66d38691
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-08-11 13:15:51 +0700

    now just copy pasting is it work?

--- heroku_app.py
+++ heroku_app.py
@@ -1,10 +1,10 @@
 import os, tempfile
-from kokoropy import bottle
 
- 
-@bottle.route("/")
-def index():
-    return "Hello World"
- 
-bottle.run(server="gevent", port=os.environ.get("PORT", 5000))
+from kokoropy.bottle import route, run
+
+@route("/")
+def hello_world():
+        return "Hello World!"
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 #kokoropy.kokoro_init( server='gevent', port=os.environ.get('PORT', 5000), application_path = './applications', run = True, runtime_path = os.path.join(tempfile.gettempdir(), '.heroku_runtime/'), base_url = '/')
