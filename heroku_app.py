abdf0246b5a433e513137e89eddce2bf164df14c
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-08-11 13:11:51 +0700

    try again

--- heroku_app.py
+++ heroku_app.py
@@ -2,9 +2,9 @@ import os, tempfile
 from kokoropy import bottle
 
  
-@bottle.route('/')
+@bottle.route("/")
 def index():
     return "Hello World"
  
-bottle.run(server='gevent', port=os.environ.get('PORT', 5000))
+bottle.run(server="gevent", port=os.environ.get("PORT", 5000))
 #kokoropy.kokoro_init( server='gevent', port=os.environ.get('PORT', 5000), application_path = './applications', run = True, runtime_path = os.path.join(tempfile.gettempdir(), '.heroku_runtime/'), base_url = '/')
