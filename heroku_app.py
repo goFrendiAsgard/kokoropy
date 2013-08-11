d4034e1278f6d827956fefa494f52972a3c3beba
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-08-11 12:52:51 +0700

    modify heroku_app

--- heroku_app.py
+++ heroku_app.py
@@ -6,12 +6,7 @@ import os, tempfile
 
 import kokoropy
 
-# PWD = os.path.dirname(os.path.abspath(__file__))
-# APP_DIRECTORY = 'applications'
-# APPLICATION_PATH = os.path.join(PWD, APP_DIRECTORY)
-
-APPLICATION_PATH = './applications'
 application = kokoropy.kokoro_init(server='gevent', port=os.environ.get('PORT', 5000),
-                                   application_path = APPLICATION_PATH, run = True, 
+                                   application_path = './applications', run = True, 
                                    runtime_path = os.path.join(tempfile.gettempdir(), '.heroku_runtime/'), 
                                    base_url = '/')
