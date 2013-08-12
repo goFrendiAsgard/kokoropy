02185687458198f18902ece55f2185b326250864
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-08-12 15:42:01 +0700

    hopefully matplotlib can be installed now

--- heroku.sh
+++ heroku.sh
@@ -1,24 +1,44 @@
-# taken from https://gist.github.com/defnull/1224387 with some changes
-# also visit https://devcenter.heroku.com/articles/python
+# Visit https://devcenter.heroku.com/articles/python
 
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# get and install heroku toolbelt
+# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+# init a git repo
+git init
+
+# login to heroku (make sure you have an account on heroku.com)
+heroku login
+
+# if need scipy etc, we should use a buildpack
+heroku create --buildpack https://github.com/dbrgn/heroku-buildpack-python-sklearn/
+
+# if you already make an heroku app without specifying buildpack, please don't worry, use this command
+# heroku config:set BUILDPACK_URL=https://github.com/dbrgn/heroku-buildpack-python-sklearn/
+
+# set a virtual env
 virtualenv env
 source env/bin/activate
+
+# install several pre-requisites python packages
 pip install gunicorn
 pip install numpy
+# matplotlib need newest distribute package
+easy_install -U distribute
 pip install matplotlib
+
+# freeze... put on requirements.txt
 pip freeze > requirements.txt
- 
+
+# make heroku_app.py installable
 chmod a+x heroku_app.py
 
+# make Procfile to run heroku_app.py
 echo "web: python heroku_app.py" > Procfile
 
-heroku login
- 
-git init
+# make runtime.txt
+echo "python-2.7.4" > runtime.txt
+
+# detect all changes and deploy by using commit & push
 git add . -A
 git commit -m "Initial commit for heroku deployment"
- 
-heroku create
 git push heroku master
