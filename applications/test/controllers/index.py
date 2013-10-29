d750c784eb9eaba116d7ebc7569e05a9ce286033
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-10-29 10:25:21 +0700

    add cookie example

--- applications/test/controllers/index.py
+++ applications/test/controllers/index.py
@@ -1,7 +1,16 @@
-from kokoropy import template
+from kokoropy import template, request, response
 
 class Default_Controller(object):
     def action_index(self):
         from applications.test.models.test_model import test_things
         test_things()
         return 'look at log'
+
+    def action_set_cookie(self):
+        # Juno is cookie name
+        # Minerva is the value
+        # Aphrodite is the secret
+        response.set_cookie('Juno', 'Minerva', 'Aphrodite')
+
+    def action_get_cookie(self):
+        return request.get_cookie('Juno', 'x', 'Aphrodite')
