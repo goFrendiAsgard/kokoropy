fdebf7663056914c1035f08ecee44a6f444d922b
goFrendiAsgard <gofrendiasgard@gmail.com>
2014-03-25 07:55:42 +0700

    fix migration

--- applications/example/migrations/add_timestamp.py
+++ applications/example/migrations/add_timestamp.py
@@ -1,12 +1,15 @@
+from alembic.migration import MigrationContext
+from alembic.operations import Operations
+from sqlalchemy import DateTime, Column
+from ..configs.db import engine
+
 # just a simple upgrade
 def upgrade():
-    from alembic.migration import MigrationContext
-    from alembic.operations import Operations
-    from sqlalchemy import DateTime
-    from ..configs.db import engine
-
     conn = engine.connect()
     ctx = MigrationContext.configure(conn)
     op = Operations(ctx)
 
     op.add_column('pokemon', Column('last_transaction_date', DateTime))
+
+def downgrade():
+    pass
