9b046ea6cdd7f228e5db8769c69abe4499796e37
goFrendiAsgard <gofrendiasgard@gmail.com>
2014-06-04 17:50:44 +0700

    implement is_ajax and var_dump

--- kokoropy/kokoro.py
+++ kokoropy/kokoro.py
@@ -18,7 +18,7 @@ if os.path.dirname(__file__) not in sys.path:
 ###################################################################################################
 # Import things
 ###################################################################################################
-import bottle, beaker, threading, time, tempfile, re, beaker.middleware
+import bottle, beaker, threading, time, tempfile, re, beaker.middleware, types
 from bottle import default_app, debug, run, static_file,\
     response, request, TEMPLATE_PATH, route, get,\
     post, put, delete, error, hook, Bottle, redirect
@@ -782,3 +782,63 @@ def migration_downgrade(application_name):
         db_session.delete(max_migration)
         db_session.commit()
 
+def is_ajax():
+    return request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+def _var_dump(variable, depth = 0, not_new_line = False):
+    padding = " "*4*depth
+    keypadding = " "*4*(depth+1)
+    first_padding = '' if not_new_line else padding
+    not_new_line = True
+    depth+= 1
+    if type(variable) == types.DictType :
+        items = []
+        for key in variable :
+            item = keypadding + "'%s' : %s" % (key,_var_dump(variable[key],depth, True))
+            items.append(item)
+        items = ',\n'.join(items)
+        return '%s<Dictionary> {\n%s\n%s}' % ( first_padding, items, padding)
+    elif type(variable) == types.ListType :
+        items = []
+        for item in variable :
+            items.append(_var_dump(item, depth))
+        items = ',\n'.join(items)
+        return '%s<List> [\n%s\n%s]' % ( first_padding, items, padding)
+    elif type(variable) == types.TupleType :
+        items = []
+        for item in variable :
+            items.append(_var_dump(item, depth))
+        items = ',\n'.join(items)
+        return '%s<Tuple> (\n%s\n%s)' % ( first_padding, items, padding)
+    elif type(variable) == types.InstanceType:
+        items = []
+        for key in dir(variable):
+            item = keypadding + "%s : %s" % (key,_var_dump(getattr(variable,key),depth, True))
+            items.append(item)
+        items = ',\n'.join(items)
+        return '%s<Instance of %s> (\n%s\n%s)' % ( first_padding, variable.__class__, items, padding)
+    elif type(variable) == types.MethodType:
+        return first_padding + '<Method>'
+    elif type(variable) == types.FunctionType:
+        return first_padding + '<Function>'
+    elif type(variable) == types.StringType:
+        return first_padding + "<String> '%s'" % variable
+    elif type(variable) == types.IntType:
+        return first_padding + "<Integer> %s" % variable
+    elif type(variable) == types.LongType:
+        return first_padding + "<Long> %s" % variable
+    elif type(variable) == types.FloatType:
+        return first_padding + "<Float> %s" % variable
+    elif type(variable) == types.BooleanType:
+        return first_padding + "<Boolean> %s" % variable
+    elif type(variable) == types.NoneType:
+        return first_padding + "<None>"
+    else:
+        return first_padding + "%s %s" % (type(variable), variable)
+
+def var_dump(variable, show = False):
+    result = _var_dump(variable)
+    if show:
+        print result
+    else:
+        return result
