e0cd354a653c4a37edc847288e828e2f00e08aee
goFrendiAsgard <gofrendiasgard@gmail.com>
2014-06-17 15:41:34 +0700

    remove sample application

--- applications/country/assets/css/readme.txt
+++
@@ -1,1 +0,0 @@
-Put your globally used css here
--- applications/country/controllers/commodity.py
+++
@@ -1,9 +0,0 @@
-from kokoropy.controller import Crud_Controller
-from ..models.country import Commodity
-
-class Commodity_Controller(Crud_Controller):
-    __model__               = Commodity
-    __application_name__    = 'country'
-    __view_directory__      = 'commodity'
-
-Commodity_Controller.publish_route()
